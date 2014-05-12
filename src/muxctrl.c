3fd980f43ed7ca29cdb69d787b2a1fe529c11694
Terje Runde <terje.runde@gmail.com>
2014-05-12 15:41:21 +0200

    Added missing ADG2 enable.

--- src/muxctrl.c
+++ src/muxctrl.c
@@ -4,17 +4,22 @@
 
 #define ADG2_A0 _BV(PB2)
 #define ADG2_A1 _BV(PB1)
+#define ADG2_EN _BV(PB0)
 
 
 void init_muxes() {
     // ADG2 address pins
     DDRB |= (1 << DDB2);
     DDRB |= (1 << DDB1);
-    // ADG3 control pin
+    // ADG2 enable pin AND ADG3 control pin
     DDRB |= (1 << DDB0);
 }
 
 void mux_set_analog_channel(const uint8_t channel) {
+    // Enable ADG2 (this in turn configures ADG3 correctly, see schematics)
+    PORTB |= ADG2_EN;
+
+    // Set appropriate address pins on ADG2
     switch (channel) {
         case A_CH_1:
             PORTB &= ~(ADG2_A0);
