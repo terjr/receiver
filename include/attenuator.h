bdbf05d054f1c57ed70de2883fc64413140a91a7
Terje Runde <terje.runde@gmail.com>
2014-05-12 17:30:20 +0200

    Added mute support.

--- include/attenuator.h
+++ include/attenuator.h
@@ -3,5 +3,5 @@
 #include <stdint.h>
 
 void att_tx(uint16_t data);
-void pulse_clock();
+void att_pulse_clock();
 
--- src/attenuator.c
+++ src/attenuator.c
@@ -6,11 +6,26 @@
 #define LOAD_SHIFT PA2
 #define DATA_IN PA3
 
+#define ATT_CH1 0x00
+#define ATT_CH2 0x01
+#define ATT_CH3 0x02
+
 
 void init_attenuator() {
     PORTA |= (1 << PA1) | (1 << PA2) | (1 << PA3);
     PORTA |= (1 << LOAD_SHIFT);
     PORTA &= ~(1 << CLOCK);
+}
+
+// Mute channel 1 and 2
+void att_mute() {
+    uint16_t att_data;
+
+    att_data = (ATT_CH1 << 8) | 0xff;
+    att_tx(att_data);
+
+    att_data = (ATT_CH2 << 8) | 0xff;
+    att_tx(att_data);
 }
 
 void att_tx(uint16_t data) {
@@ -24,14 +39,14 @@ void att_tx(uint16_t data) {
             PORTA &= ~(1 << DATA_IN);
         }
 
-        pulse_clock();
+        att_pulse_clock();
 
         data <<= 1;
     }
     PORTA |= (1 << LOAD_SHIFT);
 }
 
-void pulse_clock() {
+void att_pulse_clock() {
     PORTA |= (1 << CLOCK);
     // TODO: Delay here?
     PORTA &= ~(1 << CLOCK);
